b6a0fb8cd3a7890d5bdcbdb7b3a1698b664a1426
Gabriel Menoi <ga.gabrielmenoi@gmail.com>
2021-10-26 10:04:17 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:jokenpo/jogo.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+    runApp(MaterialApp(
+      home: jogo(),
+      debugShowCheckedModeBanner: false,
+    ));
+}
+
